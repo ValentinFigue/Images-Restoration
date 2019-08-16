316008329ff8cdabadf730c0fefc7ad37567fd61
Valou64 <valentin.figue@orange.fr>
2019-08-16 19:38:26 +0200

    Integration of RestaurationNetwork

---
+++ Dataset/dataset.py
@@ -0,0 +1,6 @@
+import torch
+
+class Dataset
+    """
+    Definition of the object Dataset which is used to train a network
+    """
---
+++ Network/network.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+
+
+class RestorationNetwork(nn.Module):
+
+    def __init__(self, in_channels = 3):
+        """
+        Initialization of the network
+        Args:
+            in_channels: Number of channels of the input
+        """
+
+        first_layer = torch.nn.Sequential(nn.Conv2d(in_channels,64,3,1,1),nn.ReLU())
+        intermediate_blocs = []
+        for num in range(18):
+            intermediate_blocs.append(nn.Conv2d(64,64,3,1,1),nn.ReLU())
+        intermediate_layers = torch.nn.Sequential(*intermediate_blocs)
+        last_layer = torch.nn.Sequential(nn.Conv2d(64,in_channels,3,1,1))
+
+        self.layers = torch.nn.Sequential(first_layer, intermediate_layers, last_layer)
+
+    def forward(self,x):
+        """
+        Forward function of the network definition
+
+        Args:
+            x: Torch tensor to run the network on
+
+        Returns:
+            Result of the inference
+
+        """
+
+        residual = x
+        x = self.layers(x)
+        x = residual + x
+
+        return x
